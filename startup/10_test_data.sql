8ebde17b26a30648c3392d13f67bea659276ad69
pnicto <ydkr789@gmail.com>
2023-04-07 21:36:17 +0530

    test: Add test data

---
+++ startup/10_test_data.sql
@@ -0,0 +1,5 @@
+set serveroutput on;
+
+execute create_new_user (1, 'John', 15, '121', 'city', 'street', '515001', 'manager', '123456789');
+
+execute create_new_user (2, 'Doe', 40, '357', 'city', 'street', '500075', 'manager', '987654321');
