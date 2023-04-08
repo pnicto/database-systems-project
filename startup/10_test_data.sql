27dba3bbca8a34dd2ecdc199e80c6af57310b508
pnicto <ydkr789@gmail.com>
2023-04-08 16:12:24 +0530

    test: Add test data for insert property record

--- startup/10_test_data.sql
+++ startup/10_test_data.sql
@@ -3,3 +3,11 @@ set serveroutput on;
 execute create_new_user (1, 'John', 15, '121', 'city', 'street', '515001', 'manager', '123456789');
 
 execute create_new_user (2, 'Doe', 40, '357', 'city', 'street', '500075', 'manager', '987654321');
+
+execute insert_property_record('residential', 1, 'Address', 100, 10, 50, 70, 50000, 'locality', date '2023-12-01', date '2024-12-01', 2001, 'flat', 1, 1, 3);
+
+execute insert_property_record('residential', 2, 'Address 2', 200, 2, 30, 60, 27100, 'locality 2', date '2023-01-01', date '2024-07-01', 1984, 'independent_house', 1, 2, 2);
+
+execute insert_property_record('commercial', 1, 'Address', 200, 2, 30, 60, 27100, 'locality', date '2023-01-01', date '2024-07-01', 1984, 'shop', 2, 1);
+
+execute insert_property_record('commercial', 2, 'Address 2', 4000, 10, 100, 160, 550055, 'locality 2', date '2011-03-01', date '2024-07-01', 1800, 'warehouse', 2, 1);
